33ca356e50b6416741e50c430ab3b51f9d3e967b
DrZesh <draubreyzesh@gmail.com>
2023-06-20 16:58:14 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,37 +1,46 @@
 #include <stdio.h>
-#define LARGEST 10000000000
 
 /**
- * main - Entry point
+ * main - fibonacci <3
  *
- * Return: Always 0 (Success)
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
  */
+
 int main(void)
 {
-	unsigned long int front_digit_1 = 0, back_digit_1 = 1, front_digit_2 = 0, back_digit_2 = 2;
-	unsigned long int carry1, carry2, carry3;
-	int count;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	printf("%lu, %lu, ", back_digit_1, back_digit_2);
-	for (count = 2; count < 98; count++)
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
 	{
-		if (back_digit_1 + back_digit_2 > LARGEST || front_digit_2 > 0 || front_digit_1 > 0)
-		{
-			carry1 = (back_digit_1 + back_digit_2) / LARGEST;
-			carry2 = (back_digit_1 + back_digit_2) % LARGEST;
-			carry3 = front_digit_1 + front_digit_2 + carry1;
-			front_digit_1 = front_digit_2, front_digit_2 = carry3;
-			back_digit_1 = back_digit_2, back_digit_2 = carry2;
-			printf("%lu%010lu", front_digit_2, back_digit_2);
-		}
-		else
-		{
-			carry2 = back_digit_1 + back_digit_2;
-			back_digit_1 = back_digit_2, back_digit_2 = carry2;
-			printf("%lu", back_digit_2);
-		}
-		if (count != 97)
-			printf(", ");
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
