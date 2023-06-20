935a8d6ac732b70e86552a8905dd654f3e4972b9
DrZesh <draubreyzesh@gmail.com>
2023-06-20 12:51:14 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabets in lowercase
+ * return; void
+ */
+void print_alphabet(void);
+{
+char lowercase_aphabet = 'a';
+while (lowercase_alphabet <= 'z')
+{
+_putchar(lowercase_alphabet);
+lowercase_alphabet++;
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to be checked
+ * Return: isletter_bool value
+ */
+int _isalpha(int c)
+{
+	char lowercase_alphabet, uppercase_alphabet;
+	int isletter_bool = 0;
+
+	lowercase_alphabet = 'a';
+	while (lowercase_alphabet <= 'z')
+	{
+		uppercase_alphabet = 'A';
+		while (uppercase_alphabet <= 'Z')
+		{
+			if (c == lowercase_alphabet || c == uppercase_alphabet)
+				isletter_bool = 1;
+			uppercase_alphabet++;
+		}
+		lowercase_alphabet++;
+	}
+	return (isletter_bool);
+}
